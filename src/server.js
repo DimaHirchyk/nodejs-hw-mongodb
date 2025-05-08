d25993af0b5a7bbe478fb4fca21e402903d20b67
Dima Hirchyk <dimzuk04@gmail.com>
2025-05-08 19:14:20 +0300

    3

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './servis/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '8080'));
 
@@ -20,12 +21,45 @@ export const setupServer = async () => {
   );
 
   app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send("It's work");
+  });
+
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        res.status(404).json({ message: 'Contact not found' });
+        return;
+      }
+
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   });
 
   app.use((req, res, next) => {
     res.status(404).json({
-      message: 'Not found',
+      message: 'Not found this contact',
     });
   });
 
