68c2ce5eab693a78d589ec33e8b89744243b4ada
Dima Hirchyk <dimzuk04@gmail.com>
2025-05-08 13:39:20 +0300

    1

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '8080'));
+
+export const setupServer = async () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
