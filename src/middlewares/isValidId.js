2a83b35f095b934ab55640f1e1e220f911e3a3c4
Dima Hirchyk <dimzuk04@gmail.com>
2025-05-24 18:20:08 +0300

    2

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,10 +2,10 @@ import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 
 export const isValidId = (req, res, next) => {
-  const { conntactId } = req.params;
+  const { contactId } = req.params;
 
-  if (!isValidObjectId(conntactId)) {
-    throw createHttpError(400, 'Bad Request');
+  if (!isValidObjectId(contactId)) {
+    throw createHttpError(400, 'Invalid contact ID format');
   }
 
   next();
