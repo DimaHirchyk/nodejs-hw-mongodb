cfe800eba7fcaa06f19a73cb916f560e405190f4
Dima Hirchyk <dimzuk04@gmail.com>
2025-06-01 16:22:53 +0300

    1

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import { getEnvVar } from './utils/getEnvVar.js';
-import contactsRouter from './routers/contacts.js';
 import { errorHandler, notFoundHandler } from './middlewares/errorHandler.js';
+import router from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '8080'));
 
@@ -12,6 +12,7 @@ export const setupServer = async () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
   // app.use(
   //   pino({
@@ -25,7 +26,7 @@ export const setupServer = async () => {
     res.send('Server is work');
   });
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use(notFoundHandler);
 
